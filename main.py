fb478b8e431163e7eb8c32cb64b999e45176f516
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-08-25 16:37:40 -0400

    B/Y Bird Added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pymunk
 
 from game_object import Bird, ColumnV, ColumnH, Pig
 from game_logic import get_impulse_vector, Point2D, get_distance
-from game_object import Slingshot
+from game_object import Slingshot, ImpulseVector
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("arcade").setLevel(logging.WARNING)
@@ -20,10 +20,8 @@ TITLE = "Angry birds"
 GRAVITY = -900
 MAX_BIRDS = 30
 
-# Longitud máxima de la línea de la resortera
 MAX_LINE_LENGTH = 150
 
-# Umbrales de puntaje para cambiar de nivel
 LEVEL_SCORE_THRESHOLDS = {
     1: 400,
     2: 700,
@@ -33,7 +31,6 @@ LEVEL_SCORE_THRESHOLDS = {
 class App(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, TITLE)
-        # self.background = arcade.load_texture("assets/img/background33.png")
         self.space = pymunk.Space()
         self.space.gravity = (0, GRAVITY)
 
@@ -57,9 +54,6 @@ class App(arcade.Window):
         self.handler.post_solve = self.collision_handler
         
         self.setup_level()
-        
-        # self.slingshot = Slingshot(300, 110)  # Ajusta la posición de la resortera
-        # self.sprites.append(self.slingshot)
 
     def clear_level(self):
         for obj in self.world:
@@ -77,10 +71,8 @@ class App(arcade.Window):
             for obj in self.world:
                 if obj.shape in arbiter.shapes:
                     if isinstance(obj, Pig):
-                        # Incrementar el puntaje por destruir un cerdo
                         self.score += 100
                     elif isinstance(obj, ColumnV) or isinstance(obj, ColumnH):
-                        # Incrementar el puntaje por destruir una columna
                         self.score += 50
 
                     obj.remove_from_sprite_lists()
@@ -88,18 +80,29 @@ class App(arcade.Window):
         return True
     
     def setup_level(self):
+        self.birds = arcade.SpriteList()
+        self.sprites = arcade.SpriteList()
+        self.world = arcade.SpriteList()
+        self.score = 0
+        self.level = 1
         self.clear_level()
+        self.slingshot = Slingshot(300, 110)  
+        self.sprites.append(self.slingshot)
+
+        self.start_point = Point2D(self.slingshot.center_x, self.slingshot.center_y)
+        self.end_point = Point2D(self.slingshot.center_x, self.slingshot.center_y)
+        
         if self.level == 1:
             self.background = arcade.load_texture("assets/img/background31.png")
             self.add_columns(start_x=600, spacing=120, num_columns=1)
             self.add_pigs(start_x=600, spacing=50, num_pigs=6)
-            self.slingshot = Slingshot(300, 110)  # Ajusta la posición de la resortera
+            self.slingshot = Slingshot(300, 110) 
             self.sprites.append(self.slingshot)
         elif self.level == 2:
             self.background = arcade.load_texture("assets/img/background32.png")
             self.add_columns(start_x=500, spacing=60, num_columns=15)
             self.add_pigs(start_x=550, spacing=55, num_pigs=5)
-            self.slingshot = Slingshot(300, 110)  # Ajusta la posición de la resortera
+            self.slingshot = Slingshot(300, 110)
             self.sprites.append(self.slingshot)
         else:
             self.background = arcade.load_texture("assets/img/background33.png")
@@ -134,44 +137,78 @@ class App(arcade.Window):
         self.update_collisions()
         self.sprites.update()
         
-        # Verificar si se debe cambiar de nivel
         self.check_level_up()
         
     def update_collisions(self):
         pass
 
     def check_level_up(self):
-        # Verificar si el puntaje ha alcanzado el umbral para el próximo nivel
         if self.score >= LEVEL_SCORE_THRESHOLDS.get(self.level, float('inf')):
             self.level += 1
             logger.info(f"Nivel alcanzado: {self.level}")
             self.setup_level()
-            # Restablecer o ajustar el puntaje según la necesidad
-            # self.score = 0  # Si quieres reiniciar el puntaje en cada nivel
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            # Comprobar si el clic está dentro de la área de la resortera
             if self.slingshot.collides_with_point((x, y)):
                 self.start_point = Point2D(self.slingshot.center_x, self.slingshot.center_y)
                 self.end_point = Point2D(self.slingshot.center_x, self.slingshot.center_y)
                 self.draw_line = True
                 logger.debug(f"Start Point: {self.start_point}")
+            else:
+                for bird in self.birds:
+                    if bird.body.velocity.length > 0 and not hasattr(bird, "divided"):
+                        #Imprimir el nombre de la imagen del pajaro
+                        if (bird.texture.name[15]=='B'):
+                            self.divide_bird(bird)
+                            bird.divided = True
+                        elif (bird.texture.name[15]=='Y'):
+                            self.double_impulse(bird)
+                        return
+
+    def double_impulse(self, bird):
+        impulse_vector = ImpulseVector(bird.body.angle, bird.body.velocity.length)
+        #Borrar el pajaro que se va a dividir
+        bird.remove_from_sprite_lists()
+        self.space.remove(bird.shape, bird.body)
+
+        #Crear un nuevo pajaro
+        new_bird = Bird("assets/img/birdYellowStarWars.png", impulse_vector, bird.center_x, bird.center_y, self.space)
+        self.sprites.append(new_bird)
+        self.birds.append(new_bird)
+
+
+    def divide_bird(self, bird):
+        offsets = [(-10, 10), (10, 10)]
+        angles = [math.radians(15), math.radians(-15)]
+
+        for offset, angle in zip(offsets, angles):
+            new_x = bird.center_x + offset[0]
+            new_y = bird.center_y + offset[1]
+            
+            new_impulse_vector = ImpulseVector(bird.body.angle + angle, bird.body.velocity.length)
+            
+            new_bird = Bird("assets/img/birdBlueStarWars.png", new_impulse_vector, new_x, new_y, self.space)
+            
+            self.sprites.append(new_bird)
+            self.birds.append(new_bird)
 
     def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
         if buttons == arcade.MOUSE_BUTTON_LEFT and self.draw_line:
             self.end_point = Point2D(x, y)
             logger.debug(f"Dragging to: {self.end_point}")
 
-    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
-        if button == arcade.MOUSE_BUTTON_LEFT and self.draw_line:
-            logger.debug(f"Releasing from: {self.end_point}")
+    def on_mouse_release(self, x, y, button, modifiers):
+        if self.draw_line:
             self.draw_line = False
-            if len(self.birds) <= MAX_BIRDS:
+            if get_distance(self.start_point, self.end_point) > 10:
                 impulse_vector = get_impulse_vector(self.start_point, self.end_point)
-                bird = Bird("assets/img/birdYellowStarWars.png", impulse_vector, self.end_point.x, self.end_point.y, self.space)
-                self.sprites.append(bird)
+                bird = Bird("assets/img/birdRedStarWars.png", impulse_vector, self.slingshot.center_x, self.slingshot.center_y, self.space)
                 self.birds.append(bird)
+                self.sprites.append(bird)
+                self.end_point = self.start_point
+                logger.debug(f"Impulse vector: {impulse_vector}")
+
 
     def on_draw(self):
         arcade.start_render()
@@ -179,10 +216,8 @@ class App(arcade.Window):
         self.sprites.draw()
         
         if self.draw_line:
-            # Calcula la distancia entre el punto de inicio y el punto final
             distance = get_distance(self.start_point, self.end_point)
             if distance > MAX_LINE_LENGTH:
-                # Ajusta el punto final si la distancia es mayor que la longitud máxima
                 direction = Point2D(self.end_point.x - self.start_point.x, self.end_point.y - self.start_point.y)
                 length = math.sqrt(direction.x**2 + direction.y**2)
                 direction.x /= length
